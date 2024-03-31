3f36e77f8fafd8755fa6d586b6eb47d634d8e000
Tashfiqul Islam <43652209+tashfiqul-islam@users.noreply.github.com>
2024-03-31 15:49:45 +0600

    updated bn24 data structure

--- src/bn24_crawler_v2.py
+++ src/bn24_crawler_v2.py
@@ -1,16 +1,18 @@
 """
-This module provides functionality to interact with JSON data.
+This module contains the BanglaNews24Crawler class which is used to crawl 
+news articles from the BanglaNews24 website.
 """
 
 import json
 from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
+import re
 import requests
+from bs4 import BeautifulSoup
 
 
 class BanglaNews24Crawler:
     """
-    A class to crawl news articles from BanglaNews24 website.
+    A class to crawl news articles from the BanglaNews24 website.
     """
 
     def __init__(self):
@@ -19,25 +21,59 @@ class BanglaNews24Crawler:
         """
         self.articles = []
 
-    def fetch_articles(self, base_url_param, start_date_param, end_date_param):
+    def fetch_all_categories(self, base_url):
         """
-        Fetches articles from the BanglaNews24 website for a specified date range.
+        Fetches all category URLs from the homepage of BanglaNews24 website.
 
         Args:
-            base_url_param (str): The base URL of the website.
+            base_url (str): The base URL of the website.
+        """
+        try:
+            print("Fetching all categories from URL:", base_url)
+            response = requests.get(base_url, timeout=5)
+            soup = BeautifulSoup(response.content, "html.parser")
+            all_category_links = self.extract_category_links(soup)
+            return all_category_links
+        except requests.exceptions.RequestException as e:
+            print("Failed to fetch categories:", e)
+
+    def extract_category_links(self, soup):
+        """
+        Extracts category URLs from the homepage soup.
+
+        Args:
+            soup (BeautifulSoup): The BeautifulSoup object of the homepage.
+
+        Returns:
+            list: List of category URLs.
+        """
+        extracted_links = []
+        categories = soup.find_all("li", class_="dropdown")
+        for category in categories:
+            category_link = category.find("a").get("href")
+            extracted_links.append(category_link)
+        return extracted_links
+
+    def fetch_articles(self, links, start_date_param, end_date_param):
+        """
+        Fetches articles from the specified category URLs for a specified date range.
+
+        Args:
+            links (list): List of category URLs.
             start_date_param (datetime): The start date of the article search range.
             end_date_param (datetime): The end date of the article search range.
         """
-        try:
-            current_date = start_date_param
-            while current_date <= end_date_param:
-                date_str = current_date.strftime("%Y/%m/%d")
-                url = f"{base_url_param}category/জাতীয়/1?date={date_str}"
-                print("Fetching articles from URL:", url)
-                self.parse_archive(url)
-                current_date += timedelta(days=1)
-        except requests.exceptions.RequestException as e:
-            print("Failed to fetch articles:", e)
+        for category_link in links:
+            try:
+                current_date = start_date_param
+                while current_date <= end_date_param:
+                    date_str = current_date.strftime("%Y/%m/%d")
+                    url = f"{category_link}?date={date_str}"
+                    print("Fetching articles from URL:", url)
+                    self.parse_archive(url)
+                    current_date += timedelta(days=1)
+            except requests.exceptions.RequestException as e:
+                print("Failed to fetch articles from category:", category_link, e)
 
     def parse_archive(self, url):
         """
@@ -77,21 +113,55 @@ class BanglaNews24Crawler:
             )
 
             # Extracting title
-            title_tag = article_soup.find("h1", class_="post-heading")
-            title = title_tag.get_text().strip() if title_tag else None
+            title_tag = article_soup.find("img", class_="lazy-load")
+            title = title_tag["alt"].strip() if title_tag else None
 
             # Extracting date
-            date_tags = article_soup.select(".news-article .time")
-            date = date_tags[0].get_text().strip() if date_tags else None
+            date_tag = article_soup.find("spam", class_="time")
+            if date_tag:
+                date_str = date_tag.get_text().strip()
+                # Remove "আপডেট:" from the date string
+                date = date_str.replace("আপডেট:", "").strip()
+            else:
+                date = None
 
             # Extracting content
-            content_tag = article_soup.find("div", {"class": "details"})
+            content_tag = article_soup.find("article")
             content_parts = content_tag.find_all("p")
-            # Combine all paragraphs to get the main content
-            content = "\n".join([p.get_text().strip() for p in content_parts])
+
+            # Initialize an empty list to hold the sentences
+            content_sentences = []
+
+            class CustomStopIteration(Exception):
+                """
+                Custom exception used to break out of nested loops.
+                """
+
+            try:
+                # Iterate over each paragraph
+                for p in content_parts:
+                    # Split the paragraph into sentences
+                    sentences = p.get_text().replace("\n", " ").strip().split(".")
+                    # Iterate over each sentence
+                    for sentence in sentences:
+                        # Find "বাংলাদেশ সময়:" or "সৌজন্যে:", stop processing this paragraph
+                        if sentence.strip().startswith(
+                            "বাংলাদেশ সময়:"
+                        ) or sentence.strip().startswith("সৌজন্যে:"):
+                            raise CustomStopIteration
+                        # Otherwise, add the sentence to the list
+                        content_sentences.append(sentence)
+            except CustomStopIteration:
+                pass
+
+            # Combine all sentences to get the main content
+            content = " ".join(content_sentences)
+
+            # Remove extra spaces
+            content = re.sub(" +", " ", content)
 
             # Extracting author
-            author_tag = article_soup.find("div", {"class": "row news-source"})
+            author_tag = article_soup.find("div", class_="row news-source")
             author = (
                 author_tag.find("span").get_text().strip().split("|")[0]
                 if author_tag
@@ -99,7 +169,7 @@ class BanglaNews24Crawler:
             )
 
             # Extracting category
-            category_tag = article_soup.find("div", {"class": "section-page-title"})
+            category_tag = article_soup.find("div", class_="section-page-title")
             category = (
                 category_tag.find("h1").get_text().strip()
                 if category_tag
@@ -142,6 +212,10 @@ if __name__ == "__main__":
     START_DATE = datetime(2024, 3, 30)  # Start date
     END_DATE = datetime(2024, 3, 30)  # End date
 
-    # Fetch articles and save to JSON
-    crawler.fetch_articles(BASE_URL, START_DATE, END_DATE)
-    crawler.save_articles_to_json("./data/banglanews24_articles.json")
+    # Fetch all categories
+    category_links = crawler.fetch_all_categories(BASE_URL)
+
+    # Fetch articles for each category and save to JSON
+    if category_links:
+        crawler.fetch_articles(category_links, START_DATE, END_DATE)
+        crawler.save_articles_to_json("./data/banglanews24_articles.json")
